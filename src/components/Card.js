162a73a0acea48ef95ec024daf1354bd08e638aa
Clem <clemence.fauchet23@gmail.com>
2020-02-20 17:12:46 +0100

    Adding flipped className

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
-function Card({ cardUrl }) {
+import Image from './Image'
+
+function Card({ cardUrl, handleClick, isFlipped }) {
   return (
-    <div className='memory-card'>
-      <img className='front-face' src={cardUrl} alt='card' />
-      <img className='back-face' src='/images/back-card.png' alt='back' />
+    <div
+      onClick={handleClick}
+      className={`memory-card ${isFlipped ? 'flipped ' : ''}`}
+    >
+      <Image className='front-face' src={cardUrl} alt='card' />
+      <Image
+        className='back-face'
+        src='static/images/back-card.png'
+        alt='back'
+      />
     </div>
   )
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import cardImages from './cardImages'
 import Card from './Card'
 
 function Game() {
-  const cards = cardImages.map((card) => (
-    <Card cardUrl={'/static/images/' + card} />
+  const [state, setState] = useState({
+    isFlipped: false,
+  })
+
+  const flipCard = () => {
+    setState({
+      ...state,
+      isFlipped: !state.isFlipped,
+    })
+  }
+
+  const cards = cardImages.map((card, index) => (
+    <Card
+      id={1}
+      cardUrl={'static/images/' + card}
+      key={index}
+      handleClick={() => flipCard()}
+      isFlipped={state.isFlipped}
+    />
   ))
 
   return <div className='gameboard'>{cards}</div>
