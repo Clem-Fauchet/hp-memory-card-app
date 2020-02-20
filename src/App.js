cd85ff7b6e4ca276cf9dbc1c9805c271b7f76215
Clem <clemence.fauchet23@gmail.com>
2020-02-20 15:07:35 +0100

    Styling gameboard

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
     <div className='App'>
       <div className='playground'>
         <h1>Memory Card</h1>
-        <h2>Can you remember where the cards are?</h2>
         <div className='gameboard'>
           <div className='memory-card'>
             <img
@@ -24,6 +23,150 @@ function App() {
               className='front-face'
               src='/images/hermione-card.png'
               alt='hermione card'
+            />
+            <img className='back-face' src='/images/back-card.png' alt='back' />
+          </div>
+
+          <div className='memory-card'>
+            <img
+              className='front-face'
+              src='/images/draco-card.png'
+              alt='draco card'
+            />
+            <img className='back-face' src='/images/back-card.png' alt='back' />
+          </div>
+
+          <div className='memory-card'>
+            <img
+              className='front-face'
+              src='/images/albus-card.png'
+              alt='albus card'
+            />
+            <img className='back-face' src='/images/back-card.png' alt='back' />
+          </div>
+
+          <div className='memory-card'>
+            <img
+              className='front-face'
+              src='/images/snape-card.png'
+              alt='snape card'
+            />
+            <img className='back-face' src='/images/back-card.png' alt='back' />
+          </div>
+
+          <div className='memory-card'>
+            <img
+              className='front-face'
+              src='/images/ron-card.png'
+              alt='ron card'
+            />
+            <img className='back-face' src='/images/back-card.png' alt='back' />
+          </div>
+
+          <div className='memory-card'>
+            <img
+              className='front-face'
+              src='/images/harry-card.png'
+              alt='hp card'
+            />
+            <img className='back-face' src='/images/back-card.png' alt='back' />
+          </div>
+
+          <div className='memory-card'>
+            <img
+              className='front-face'
+              src='/images/hermione-card.png'
+              alt='hermione card'
+            />
+            <img className='back-face' src='/images/back-card.png' alt='back' />
+          </div>
+
+          <div className='memory-card'>
+            <img
+              className='front-face'
+              src='/images/draco-card.png'
+              alt='draco card'
+            />
+            <img className='back-face' src='/images/back-card.png' alt='back' />
+          </div>
+
+          <div className='memory-card'>
+            <img
+              className='front-face'
+              src='/images/albus-card.png'
+              alt='albus card'
+            />
+            <img className='back-face' src='/images/back-card.png' alt='back' />
+          </div>
+
+          <div className='memory-card'>
+            <img
+              className='front-face'
+              src='/images/snape-card.png'
+              alt='snape card'
+            />
+            <img className='back-face' src='/images/back-card.png' alt='back' />
+          </div>
+
+          <div className='memory-card'>
+            <img
+              className='front-face'
+              src='/images/ron-card.png'
+              alt='ron card'
+            />
+            <img className='back-face' src='/images/back-card.png' alt='back' />
+          </div>
+
+          <div className='memory-card'>
+            <img
+              className='front-face'
+              src='/images/harry-card.png'
+              alt='hp card'
+            />
+            <img className='back-face' src='/images/back-card.png' alt='back' />
+          </div>
+
+          <div className='memory-card'>
+            <img
+              className='front-face'
+              src='/images/hermione-card.png'
+              alt='hermione card'
+            />
+            <img className='back-face' src='/images/back-card.png' alt='back' />
+          </div>
+
+          <div className='memory-card'>
+            <img
+              className='front-face'
+              src='/images/draco-card.png'
+              alt='draco card'
+            />
+            <img className='back-face' src='/images/back-card.png' alt='back' />
+          </div>
+
+          <div className='memory-card'>
+            <img
+              className='front-face'
+              src='/images/albus-card.png'
+              alt='albus card'
+            />
+            <img className='back-face' src='/images/back-card.png' alt='back' />
+          </div>
+
+          <div className='memory-card'>
+            <img
+              className='front-face'
+              src='/images/snape-card.png'
+              alt='snape card'
+            />
+            <img className='back-face' src='/images/back-card.png' alt='back' />
+          </div>
+
+          <div className='memory-card'>
+            <img
+              className='front-face'
+              src='/images/ron-card.png'
+              alt='ron card'
             />
             <img className='back-face' src='/images/back-card.png' alt='back' />
           </div>
